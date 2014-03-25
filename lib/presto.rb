89b2837e12dfeb11485864100bb2aab5f596ffde
JP Simard <jp@jpsim.com>
2014-03-25 17:24:29 -0400

    Started to add new method: load_card

--- lib/presto.rb
+++ lib/presto.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'mechanize'
+require 'open-uri'
+require 'json'
+require 'base64'
 
 module Presto
   class CardStatus
@@ -34,6 +37,13 @@ module Presto
       :loyalty_discount
   end
 
+  class CreditCard
+    attr_accessor :name,
+      :number,
+      :expiry_month,
+      :expiry_year
+  end
+
   class PrestoAPI
     def user_with_username_password(username, password)
       login_with_username_password(username, password)
@@ -51,6 +61,16 @@ module Presto
 
     def card_status_with_number(card_number)
       card_status_from_page(login_with_card_number(card_number))
+    end
+
+    def load_registered_card(username, password, email, amount, credit_card)
+      login_with_username_password(username, password)
+      load_card(amount, email, credit_card)
+    end
+
+    def load_unregistered_card(card_number, email, amount, credit_card)
+      login_with_card_number(card_number)
+      load_card(amount, email, credit_card)
     end
 
     private
@@ -152,6 +172,77 @@ module Presto
         transaction_history.push transaction
       end
       transaction_history
+    end
+
+    def load_card(amount, email, credit_card)
+      # Check that we want to load a dollar value onto the card
+      page = agent.get('https://www.prestocard.ca/en-US/Pages/TransactionalPages/ReloadEpurse.aspx')
+      form = page.forms.first
+      radio_button = form.radiobutton_with(:id => 'ctl00_SPWebPartManager1_LoadePurseWebPartWebpartControl_ctl00_wizardLoadEPurse_webpartPeriodPassWebPart_ctl00_wizardPeriodPassSelection_radioButtonListProducts_0')
+      radio_button.check
+      button = form.button_with(:name => "ctl00$SPWebPartManager1$LoadePurseWebPartWebpartControl$ctl00$wizardLoadEPurse$webpartPeriodPassWebPart$ctl00$wizardPeriodPassSelection$buttonNextinProductSelection")
+      page = agent.submit(form, button)
+
+      # Enter the amount to add to the card and our email address
+      form = page.forms.first
+      amount_field = form.field_with(:id => "ctl00_SPWebPartManager1_LoadePurseWebPartWebpartControl_ctl00_wizardLoadEPurse_textBoxamountToReload")
+      amount_field.value = amount
+      email_field = form.field_with(:id => "ctl00_SPWebPartManager1_LoadePurseWebPartWebpartControl_ctl00_wizardLoadEPurse_textBoxemailAddress")
+      email_field.value = email
+      button = form.button_with(:name => "ctl00$SPWebPartManager1$LoadePurseWebPartWebpartControl$ctl00$wizardLoadEPurse$buttonConfirmReload")
+      page = agent.submit(form, button)
+
+      # Confirm Amount
+      form = page.forms.first
+      button = form.button_with(:name => "ctl00$SPWebPartManager1$LoadePurseWebPartWebpartControl$ctl00$wizardLoadEPurse$buttonConfirmInVerification")
+      page = agent.submit(form, button)
+
+      # Intermediate Moneris Submit
+      storeId = page.body[/storeId = '[^\s]*'/,0][/'.*'/,0][1..-2]
+      hppKey = page.body[/hppKey = '[^\s]*'/,0][/'.*'/,0][1..-2]
+      totalCharge = page.body[/totalCharge = '[^\s]*'/,0][/'.*'/,0][1..-2]
+      orderId = page.body[/orderId = '[^\s]*'/,0][/'.*'/,0][1..-2]
+      language = page.body[/language = '[^\s]*'/,0][/'.*'/,0][1..-2]
+
+      form = page.forms.first
+      form.field_with(:id => "ps_store_id").value = storeId
+      form.field_with(:id => "hpp_key").value = hppKey
+      form.field_with(:id => "charge_total").value = totalCharge
+      form.field_with(:id => "order_id").value = orderId
+      form.field_with(:id => "lang").value = language
+      form.action = "https://www3.moneris.com/HPPDP/index.php"
+      page = form.submit
+
+      # Enter Payment Details
+      callCCPurchase = page.body[/function callCCPurchase\(\)[^}]*/,0]
+
+      hpp_id = callCCPurchase[/hpp_id=[^"]+/,0]
+      hpp_id["hpp_id="] = ""
+      hpp_ticket = callCCPurchase[/hpp_ticket=[^"]+/,0]
+      hpp_ticket["hpp_ticket="] = ""
+      pan = credit_card_number
+      pan_mm = credit_card.expiry_month
+      pan_yy = credit_card.expiry_year
+      cardholder = credit_card.name
+
+      post_data = "hpp_id=" + CGI::escape(hpp_id) +
+        "&hpp_ticket=" + CGI::escape(hpp_ticket) +
+        "&pan=" + CGI::escape(pan) +
+        "&pan_mm=" + CGI::escape(pan_mm) +
+        "&pan_yy=" + CGI::escape(pan_yy) +
+        "&cardholder=" + CGI::escape(cardholder) +
+        "&doTransaction=cc_purchase"
+
+      # Send Payment
+      headers = { 'Content-Type' => 'application/x-www-form-urlencoded', 'Connection' => 'close', 'Content-length' => post_data.length }
+      page = agent.post( 'https://www3.moneris.com/HPPDP/hprequest.php', post_data, headers)
+
+      # Confirm with Credit Card Provider
+      parsed = JSON.parse(page.body)
+      form_text_encoded = parsed['response']['data']['form']
+      form_text_decoded = Base64.decode64(form_text_encoded)
+
+      # TODO: continue this method
     end
 
     def agent
